a2ddb51833afb30982f436a09660f5ac13898db3
linyaohong <you@example.com>
2019-11-29 18:35:14 +0800

    lyh

--- Docker/docker_install.sh
+++ Docker/docker_install.sh
@@ -20,6 +20,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 #Daocloud镜像
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
 ln -s /usr/local/bin/docker-compose /usr/bin/
 
 ./prepare 
